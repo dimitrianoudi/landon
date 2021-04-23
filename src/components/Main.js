afd7ca8b96ac0334cd9ebd4f2630e7e240dbd638
Dimitri Anoudi <dimitrianoudi@gmail.com>
2021-04-23 19:22:34 +0300

    Complete react app with json mock

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Welcome from './Welcome';
+import Hotelinfo from './Hotelinfo';
+
+const Main = () => {
+
+	return (
+
+		<main id="wrapper">
+			<Welcome />
+			<Hotelinfo />
+
+		</main>
+
+	);
+
+}
+
+export default Main;
